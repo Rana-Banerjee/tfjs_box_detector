9b92bc274eeb088a278ccf4261a2cd54c16f0208
Rana Banerjee <rana1224@gmail.com>
2021-07-11 13:00:20 +0530

    Azure web app deployment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# tfjs_box_detector
+# Tensorflowjs client side web app for detecting boxes
 Tensorflow js model to dtect boxes running on client side (webapp)
+
+# Steps to Execute
+1. From the base folder(The one contatining package.json) run the following command to install the necessary node packages: 
+<br> npm install
+2. Run the following command from the base folder:
+<br> npm start
+3. A browser will open/ Else open a browser with path as http://localhost:3000 on which the box detector would run
---
+++ build/precache-manifest.c3b6082a2858272df2ec7cb7ef7b15d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8494f1ffdc886540f91d",
+    "url": "/static/css/main.0946d362.chunk.css"
+  },
+  {
+    "revision": "8494f1ffdc886540f91d",
+    "url": "/static/js/main.8494f1ff.chunk.js"
+  },
+  {
+    "revision": "dcb9cd2782069964c890",
+    "url": "/static/js/1.dcb9cd27.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fd69882fda7e223d7e300dd22c102b71",
+    "url": "/index.html"
+  }
+];
